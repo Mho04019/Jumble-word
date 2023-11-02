df7e0c85ffdd588449f9d138517490279f0425d7
Dechen Lhendup DOS <dlhendup@audit.local>
2023-11-03 01:32:01 +0600

    initial setup

---
+++ my_game.py
@@ -0,0 +1,23 @@
+# Program: Jumbled Word Game
+
+import random
+
+words = ["python", "coding", "almost",
+         "before", "attack", "chance"]
+
+word = random.choice(words)
+
+letters = list(word)
+random.shuffle(letters)
+jumbled_word = "".join(letters)
+
+print("Unscramble...", jumbled_word)
+
+while True:
+    guess = input("Guess: ")
+    if guess.lower() != word.lower():
+        print("Incorrect try again!")
+    else:
+        break
+
+print("Congratulations!")
